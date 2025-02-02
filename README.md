485ec47d935f53162419c570a0b54ea40b6333d4
pwalczak418 <150608424+pwalczak418@users.noreply.github.com>
2025-02-02 14:05:19 +0100

    v1.0 - Initial commit

--- README.md
+++ README.md
@@ -1,2 +1,8 @@
 # shoperapi-handytools
-Handy tools for Shoper API with UI
+Handy tools for Shoper API with UI in Python
+
+Version History
+v1.0 - Initial release. Added tool for deleting attributes and attribute groups (up to 50 per iteration)
+
+License
+MIT License
---
+++ handytools.py
@@ -0,0 +1,105 @@
+#v1.0
+
+import tkinter as tk
+from tkinter import ttk
+from requests.auth import HTTPBasicAuth
+import time
+import requests
+
+global url #Complete URL for store's API
+
+session_shoper = requests.Session()
+session_shoper.headers.update({
+    'User-Agent': 'Handytools/1.0 (+https://github.com/pwalczak418/shoperapi-handytools; https://pwalczak.net)',
+    "Content-Type": "application/json",
+})
+
+def login():
+    global url
+    url = entry_shpr_url.get()
+    url = url if url.startswith("https://") else "https://" + url.replace("http://", "")
+    url = url if url.endswith("/") else url + "/"
+    url = url + "webapi/rest/"
+
+    shpr_user = entry_shpr_user.get()
+    shpr_pass = entry_shpr_pass.get()
+
+    loginurl = url + "auth"
+    response = session_shoper.post(loginurl, auth=HTTPBasicAuth(shpr_user, shpr_pass))
+    print(response.status_code)
+    print(response.text)
+    if response.status_code == 200:
+        response_data = response.json()
+        bearer_token = response_data['access_token']
+        session_shoper.headers.update({
+        'Authorization': 'Bearer ' + bearer_token
+                        })
+        print(f"Login was successful: {url}")
+        button_login.destroy()
+        label_login = ttk.Label(root, text=f"Logged in, Shoper URL: {url}")
+        label_login.pack()
+        create_menu()
+    else:
+        print(f"Login was unsuccessful, HTTP code: {response.status_code}")
+
+def create_menu():
+    delete_attributes_button = ttk.Button(root, text="Delete attributes and attribute groups", command=delete_attributes)
+    delete_attributes_button.pack(pady=2)
+
+######################################################################
+#                              TOOLS                                 #
+######################################################################
+
+def delete_attributes(): #Up to 50 for this version
+    att__url = url + "attributes"
+    gr_att_url = url + "attribute-groups"
+    response1 = session_shoper.get(att__url, params={"limit": 50})
+    response2 = session_shoper.get(gr_att_url, params={"limit": 50})
+    data1 = response1.json()
+    data2 = response2.json()
+
+    for item in data1["list"]:
+        att_id = item["attribute_id"]
+        session_shoper.delete(f"{att__url}/{att_id}")
+        time.sleep(1)
+
+    for item in data2["list"]:
+        gr_att_id = item["attribute_group_id"]
+        json = {"categories":[]} # Categories must be deleted from the attribute group before the group can be deleted.
+        session_shoper.put(f"{gr_att_url}/{gr_att_id}", json=json)
+        time.sleep(1)
+        session_shoper.delete(f"{gr_att_url}/{gr_att_id}")
+        time.sleep(1)
+
+######################################################################
+#                                UI                                  #
+######################################################################
+
+root = tk.Tk()
+root.title("Handytools for Shoper API")
+root.geometry("600x400")
+root.resizable(False, False)
+root.configure(bg="#0064b0")
+
+style = ttk.Style()
+style.configure("TLabel", background="#9cd4ff")
+
+label_shpr_url = ttk.Label(root, text="Shoper URL:")
+label_shpr_url.pack()
+entry_shpr_url = ttk.Entry(root, width=30)
+entry_shpr_url.pack()
+
+label_shpr_user = ttk.Label(root, text="API user:")
+label_shpr_user.pack()
+entry_shpr_user = ttk.Entry(root, width=30)
+entry_shpr_user.pack()
+
+label_shpr_pass = ttk.Label(root, text="Password:")
+label_shpr_pass.pack()
+entry_shpr_pass = ttk.Entry(root, width=30, show="*")
+entry_shpr_pass.pack()
+
+button_login = ttk.Button(root, text="Login", command=login)
+button_login.pack(pady=2)
+
+root.mainloop()
